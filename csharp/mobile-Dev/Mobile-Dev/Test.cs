f8f341f28c7adf4425638436d6eef644555352ad
Charles Wang <charles.wang@xamarin.com>
2015-04-13 10:30:40 -0700

    Notes free demo

--- csharp/mobile-Dev/Mobile-Dev/Test.cs
+++ csharp/mobile-Dev/Mobile-Dev/Test.cs
@@ -5,20 +5,14 @@ using Xamarin.UITest.Android;
 
 namespace MobileDev.Android
 {
-    // Tests are run through NUnit.
     [TestFixture()]
     public class SimpleTest
     {
         IApp app;
 
-        /* The SetUp attribute will mark the method to run before every 
-         * Test method. A TearDown method can also be added to run 
-         * after a Test method regardless of it passing or failing. */
         [SetUp]
         public void Setup()
         {
-            /* You'll need an ApiKey to run on emulators for more than 15 min 
-            or on any physical device */
             app = ConfigureApp
                 .Android
                 .ApkFile(Constants.ApkPath)
@@ -26,18 +20,13 @@ namespace MobileDev.Android
                 .StartApp();
         }
 
-        /* The Test attribute marks a method to be tested. A TestCase
-         * attribute can be added to run a Test method with varying parameters*/
         [Test()]
         public void FindingUs()
         {
             app.Screenshot("App is open");
-            /* The Intial method is a custom written extension method. 
-             * You can find the method in Extension.cs. Try your own!*/
             app.Initial();
 
             app.Tap(x => x.Text("Exhibitors"));
-            // WaitFor's are assertions. Use them to line up screenshots too!
             app.WaitForElement(x => x.Text("Exhibitors by Name"));
             app.Screenshot("Exhibitors search open");
 
@@ -55,18 +44,14 @@ namespace MobileDev.Android
             }
             app.Screenshot("Scrolled thrice for the Xamarin name");
 
-            /* Marked is a nice shortcut and will look for matching 
-            text in the Id, Text, or Label attributes */
+
             app.Tap(x => x.Marked("Search"));
-            /* Intellisence will show you different options! The EnterText method
-             * can select then type or type into an already selected field. */
             app.EnterText(x => x.Id("search_bar"), "Xamarin");
             app.PressEnter();
             app.Screenshot("Searching for Xamarin with the search bar");
 
             app.Tap(x => x.Id("list_item_title").Text("Xamarin"));
             app.WaitForElement(x => x.Text("Xamarin"));
-            // Screenshots are only taken on the cloud and are paired with the text you insert here.
             app.Screenshot("Xamarin page");
         }
     }
